a8ae6abbc8fe244c8778453a919c678dfa9203c4
SamuelLeiva <leiva20152@gmail.com>
2025-05-12 10:53:30 -0500

    Fix: deleteProduct test fixed.

--- src/tests/use-cases/product/deleteProduct.test.ts
+++ src/tests/use-cases/product/deleteProduct.test.ts
@@ -1,11 +1,12 @@
 import { describe, it, expect, vi } from "vitest";
 import { DeleteProductUseCase } from "@/core/use-cases/product/deleteProduct";
 import { ProductRepository } from "@/core/ports";
+import { v4 as uuidv4 } from "uuid";
 
 describe("DeleteProductUseCase", () => {
   it("should delete product by id", async () => {
     const mockRepo: ProductRepository = {
-      delete: vi.fn(async (id: string) => {}),
+      delete: vi.fn(async (id: string) => {console.log(`Producto con id ${id} eliminado.`)}),
       list: vi.fn(),
       create: vi.fn(),
       getById: vi.fn(),
@@ -13,8 +14,10 @@ describe("DeleteProductUseCase", () => {
       update: vi.fn(),
     };
 
+    const id = uuidv4();
+
     const useCase = new DeleteProductUseCase(mockRepo);
-    await useCase.execute("some-id");
-    expect(mockRepo.delete).toHaveBeenCalledWith("some-id");
+    await useCase.execute(id);
+    expect(mockRepo.delete).toHaveBeenCalledWith(id);
   });
 });
