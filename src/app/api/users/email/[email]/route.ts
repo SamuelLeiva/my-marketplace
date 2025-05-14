97724c38d5507329db658bb908d2df3d2057bc4e
SamuelLeiva <leiva20152@gmail.com>
2025-05-14 15:46:51 -0500

    Chore: GetUserByEmail route added.

--- src/app/api/users/email/[email]/route.ts
+++ src/app/api/users/email/[email]/route.ts
@@ -1,25 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import { GetUserByIdUseCase } from "@/core/use-cases/user/getUserById";
 import { PrismaUserRepository } from "@/infrastructure/db/prisma/userRepository";
+import { GetUserByEmailUseCase } from "@/core/use-cases/user";
 
 interface Params {
   params: {
-    id: string;
+    email: string;
   };
 }
 
 // GET: Obtener user por ID
 export async function GET(req: NextRequest, { params }: Params) {
   try {
-    const useCase = new GetUserByIdUseCase(PrismaUserRepository);
-    const user = await useCase.execute(params.id);
+    const useCase = new GetUserByEmailUseCase(PrismaUserRepository);
+    const user = await useCase.execute(params.email);
     if (user) {
       return NextResponse.json(user);
     } else {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
   } catch (error) {
-    console.error("GET /api/users/[id] error:", error);
+    console.error("GET /api/users/[email] error:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }
